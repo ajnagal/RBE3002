c12fb351de975c1c6c3f0265fd178bcdcab823e1
SpeedFreed <speedfreed@gmail.com>
2016-11-15 00:01:46 -0500

    wheeee

---
+++ lab3/scripts/a_star.py
@@ -0,0 +1,79 @@
+
+import numpy
+from heapq import *
+
+
+def heuristic(a, b):
+    #print("a: ", a,"b: ", b)
+    return (b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2
+
+def astar(array, start, goal, wall_value, step):
+    print("Start is:", start, "goal is", goal)
+    array = numpy.swapaxes(array,0,1)
+    neighbors = [(0,step),(0,-step),(step,0),(-step,0),(step,step),(step,-step),(-step,step),(-step,-step)]
+
+    close_set = set()
+    came_from = {}
+    gscore = {start:0}
+    fscore = {start:heuristic(start, goal)}
+    oheap = []
+
+    heappush(oheap, (fscore[start], start))
+    
+    while oheap:
+
+        current = heappop(oheap)[1]
+
+        if current == goal:
+            data = []
+            while current in came_from:
+                data.append(current)
+                current = came_from[current]
+            return data
+
+        close_set.add(current)
+        for i, j in neighbors:
+            neighbor = current[0] + i, current[1] + j            
+            tentative_g_score = gscore[current] + heuristic(current, neighbor)
+            if 0 <= neighbor[0] < array.shape[0]:
+                if 0 <= neighbor[1] < array.shape[1]:
+                    #print(array[neighbor[0]][neighbor[1]])
+                    #if array[neighbor[0]][neighbor[1]] == wall_value:
+                    if array[neighbor[0]][neighbor[1]] >= wall_value or array[neighbor[0]][neighbor[1]] == -1:
+                        continue
+                else:
+                    # array bound y walls
+                    continue
+            else:
+                # array bound x walls
+                continue
+                
+            if neighbor in close_set and tentative_g_score >= gscore.get(neighbor, 0):
+                continue
+                
+            if  tentative_g_score < gscore.get(neighbor, 0) or neighbor not in [i[1]for i in oheap]:
+                came_from[neighbor] = current
+                gscore[neighbor] = tentative_g_score
+                fscore[neighbor] = tentative_g_score + heuristic(neighbor, goal)
+                heappush(oheap, (fscore[neighbor], neighbor))
+                
+    return False
+
+'''Here is an example of using my algo with a numpy array,
+   astar(array, start, destination)
+   astar function returns a list of points (shortest path)'''
+
+nmap = numpy.array([
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    ['a','a','a','a','a','a','a','a','a','a','a','a',0,'a'],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    ['a','a','a','a','a','a','a',0,'a','a','a','a','a','a'],
+    [0,0,0,'a',0,0,0,0,0,0,0,0,0,0],
+    ['a','a','a','a',0,'a','a','a','a','a','a','a',0,'a'],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    ['a',0,'a','a','a','a','a','a','a','a','a','a','a','a'],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    ['a','a','a','a','a','a','a','a','a','a','a','a',0,'a'],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0]])
+    
+#print (type(numpy.array(astar(nmap, (0,0), (10,13),'a'))))
---
+++ lab3/scripts/lab3_grid_cellsB16.py
@@ -0,0 +1,143 @@
+    #!/usr/bin/env python
+
+import rospy
+from nav_msgs.msg import GridCells
+from std_msgs.msg import String
+from geometry_msgs.msg import Twist, Point, PointStamped, Pose, PoseStamped, PoseWithCovarianceStamped
+from nav_msgs.msg import Odometry, OccupancyGrid
+from kobuki_msgs.msg import BumperEvent
+import tf
+import numpy
+import math 
+import rospy, tf, numpy, math
+from a_star import astar
+
+
+
+# reads in global map
+def mapCallBack(data):
+    global mapData
+    global width
+    global height
+    global mapgrid
+    global resolution
+    global offsetX
+    global offsetY
+    mapgrid = data
+    resolution = data.info.resolution
+    mapData = data.data
+    width = data.info.width
+    height = data.info.height
+    offsetX = data.info.origin.position.x
+    offsetY = data.info.origin.position.y
+    print data.info
+
+def readGoal(goal):
+    global goalX
+    global goalY
+    goalX= goal.pose.position.x
+    goalY= goal.pose.position.y
+    print "Goal: "
+    print goal.pose
+    star()
+
+
+def readStart(startPoint):
+
+    global startPosX
+    global startPosY
+    startPosX = startPoint.point.x
+    startPosY = startPoint.point.y
+    print "Start: "
+    print startPoint.point
+
+def star():
+    global mapData
+    global startPosX
+    global startPosY
+    global goalX
+    global goalY
+    global width
+    global height
+    # create a new instance of the map
+    myMap = []
+    temp = mapData
+    for j in range(0, height):
+        temp1 = []
+        for i in range(0, width):
+            temp1.append(temp.pop(0))
+        myMap.append(temp1)
+
+
+    #readGoal(goal)
+    #readStart(start)
+    myStart = (startPosX, startPosY)
+    myGoal = (goalX, goalY)
+
+    # generate a path to the start and end goals by searching through the neighbors, refer to aStar_explanied.py
+    tup_path = astar(myMap, myStart, myGoal, 20, 1)
+    # for each node in the path, process the nodes to generate GridCells and Path messages
+    point_path = []
+    for i in range(0, len(tup_path)):
+        temp = Point()
+        temp.x = tup_path[i][0]
+        temp.y = tup_path[i][1]
+        temp.z = 0
+        point_path.append(temp)
+    # Publish points
+    publishCells(point_path)
+
+#publishes map to rviz using gridcells type
+
+def publishCells(grid):
+    global pub
+    print "publishing"
+
+    # resolution and offset of the map
+    k=0
+    cells = GridCells()
+    cells.header.frame_id = 'map'
+    cells.cell_width = resolution 
+    cells.cell_height = resolution
+
+    for i in range(1,height): #height should be set to height of grid
+        k=k+1
+        for j in range(1,width): #width should be set to width of grid
+            k=k+1
+            #print k # used for debugging
+            if (grid[k] == 100):
+                point=Point()
+                point.x=(j*resolution)+offsetX + (1.5 * resolution) # added secondary offset 
+                point.y=(i*resolution)+offsetY - (.5 * resolution) # added secondary offset ... Magic ?
+                point.z=0
+                cells.cells.append(point)
+    pub.publish(cells)           
+
+#Main handler of the project
+def run():
+    global pub
+    rospy.init_node('lab3')
+    sub = rospy.Subscriber("/map", OccupancyGrid, mapCallBack)
+    pub = rospy.Publisher("/map_check", GridCells, queue_size=1)  
+    pubpath = rospy.Publisher("/path", GridCells, queue_size=1) # you can use other types if desired
+    pubway = rospy.Publisher("/waypoints", GridCells, queue_size=1)
+    goal_sub = rospy.Subscriber('/move_base_simple/goal', PoseStamped, readGoal, queue_size=1) #change topic for best results
+    start_sub = rospy.Subscriber('/clicked_point', PointStamped, readStart, queue_size=1) #change topic for best results
+
+    # wait a second for publisher, subscribers, and TF
+    rospy.sleep(1)
+
+
+
+    while (1 and not rospy.is_shutdown()):
+        publishCells(mapData) #publishing map data every 2 seconds
+        rospy.sleep(2)  
+        print("Complete")
+    
+
+
+if __name__ == '__main__':
+    try:
+        run()
+    except rospy.ROSInterruptException:
+        pass
